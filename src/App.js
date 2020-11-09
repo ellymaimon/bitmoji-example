658baf3f4567b8575715df91cb24484914b46af0
ellymaimon <eliranmaimon@gmail.com>
2020-11-08 20:53:33 -0800

    update app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import initialComments from "./initialComments";
+import CommentList from "./CommentList";
 
 function App() {
+  const [comments, setComments] = useState(initialComments)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <CommentList comments={comments} setComments={setComments}/>
+    );
 }
 
 export default App;
