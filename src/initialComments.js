c08563d0e818736df8c7de03d9c37664c11223ad
ellymaimon <eliranmaimon@gmail.com>
2020-11-08 20:52:59 -0800

    add initial comment list

---
+++ src/initialComments.js
@@ -0,0 +1,22 @@
+const initialComments = [
+	{
+		userName: "Matthew Togglefritz",
+		date: new Date('August 19, 2019 23:15:30'),
+		text: "How lovely!",
+		bitmojiURL: ""
+	},
+	{
+		userName: "Samantha Globbert",
+		date: new Date('March 2, 2020 18:01:30'),
+		text: "What an excercise in fun times and good vibes.",
+		bitmojiURL: ""
+	},
+	{
+		userName: "Elegrond McDermott",
+		date: new Date(),
+		text: "Happy Halloween!",
+		bitmojiURL: "https://sdk.bitmoji.com/render/panel/10213906-AW84UlphPusoGXuXwVQSLCnYlXowgg-v1.png?transparent=1&palette=1"
+	},
+]
+
+export default initialComments;
